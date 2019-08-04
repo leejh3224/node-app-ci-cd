07412a66efb1a84ed086b6025eafbf1334b4ae26
gompro <junhyung3224@gmail.com>
2019-08-04 19:19:10 +0900

    add start node server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-const express = require("express");
+const app = require("./app");
 
-const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello world!");
+app.listen(port, () => {
+  console.log(`listening on port ${port}!`);
 });
-
-module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const app = require("../src");
+const app = require("../src/app");
 
 describe("GET /", () => {
   let server;
